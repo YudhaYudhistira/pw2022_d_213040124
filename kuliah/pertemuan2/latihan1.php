23922568d0485a5fbf8c4a18af927ef970ea61cd
YudhaYudhistira <yudhistirayudha09@gmail.com>
2022-02-24 10:52:46 +0700

    mengerjakan tugas pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,39 @@
+<?php 
+// Pertemuan 2, belajar mengenai sintaks PHP
+// echo untuk mencetak nilai ke layar 
+echo (1 + 2) * 3 / 4;
+echo "<hr>";
+// OPERATOR
+// Aritmatika
+// +,-,*,/,%
+echo 5 % 2; //sisa bagi
+
+// Perbandiangan
+// <, >, <=, >=, ==, !=
+echo 10 != 20;
+echo "<hr>";
+
+// Variabel
+// Tempat menampung nilai
+// tidak boleh menampung spasi 
+// boleh mengandung angka, tidak boleh di awal
+// Snake_case : $nama_depan_mahasiswa
+// CamelCase : namaDepanMahasiswa
+// kebab-case : nama-depan-mahasiswa
+
+
+
+
+// Penugasan / Assignment
+// =,+=, -=, *=, ?=, %=
+
+?>
+
+<!-- tanda baca -->
+<!-- ` back tick
+~ tilde
+# pound
+^ caret 
+| pipe 
+\ backslash 
+/ slash  -->
