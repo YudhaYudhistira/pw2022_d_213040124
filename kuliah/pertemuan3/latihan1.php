b8ec59d2c7960604b6190ecd7a5bbcec011982d2
YudhaYudhistira <yudhistirayudha09@gmail.com>
2022-02-25 21:49:31 +0700

    mengerjakan latihan 1, pertemuan 3

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,42 @@
+<?php
+// Pengulangan
+// for
+// while 
+// do.. while 
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++) { 
+//     echo "Hello  World! <br>";
+// }
+// echo "<hr>";
+// while( $i < 5 ){
+//     echo "Hello World! <br>";
+// $i++;
+// }
+// echo "<hr>";
+// $i = 10;
+// do {
+//     echo "Hello World! <br>";
+// $i++;
+// } while ( $i < 5 )
+// ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1 </title>
+</head>
+<body>
+    
+    <table border="1" cellpadding="10" cellpacing="0">
+        <?php for( $i = 1; $i <= 3; $i++) { ?>
+
+        <?php } ?>
+        
+    </table>
+ 
+</body>
+</html>
