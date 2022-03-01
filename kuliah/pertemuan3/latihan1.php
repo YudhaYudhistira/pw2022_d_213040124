db54dc570edeb6098f1c80a950d4ed0b3a0b6c40
ydhydhstr <yudhistirayudha09@gmail.com>
2022-03-01 15:30:54 +0700

    menyelesaikan latihan 1 pertemuan 3

--- kuliah/pertemuan3/latihan1.php
+++ kuliah/pertemuan3/latihan1.php
@@ -28,16 +28,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1 </title>
+    <style> 
+        .warna-baris {
+            background-color: silver;
+
+        }
+    </style>
 </head>
 <body>
     
     <table border="1" cellpadding="10" cellpacing="0">
-        <?php for( $i = 1; $i <= 3; $i++) : ?>
-
-            <tr>
-                <?php for( $j = 1; $j <= 5; $j++ ) { ?>
-                <td><?php echo "$i, $j"; ?></td>
-                <?php } ?>
+        <?php for( $i = 1; $i <= 5; $i++) : ?>
+            <?php if( $i % 2 == 1 ) : ?>
+            <tr class="warna-baris">
+                <?php else: ?>
+                    <tr>
+                <?php endif; ?>
+                    <?php for( $j = 1; $j <= 5; $j++ ) : ?>
+                         <td><?php echo "$i, $j"; ?></td>
+                    <?php endfor; ?>
             </tr>
         <?php endfor; ?>
     </table>
