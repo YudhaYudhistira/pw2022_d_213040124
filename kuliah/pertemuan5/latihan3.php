17b33bfaa234793163eaea7ae91ff9da54c1ce86
ydhydhstr <yudhistirayudha09@gmail.com>
2022-03-11 18:25:21 +0700

    mengerjakan tugas pertemuan 5

---
+++ kuliah/pertemuan5/latihan3.php
@@ -0,0 +1,26 @@
+<?php
+// Representasi Data Mahasiswa
+
+$mahasiswa = [
+["Yudha Yudhistira", "213040124",
+"yudhistirayudha09@gmail.com","Teknik Informatika"],
+["Vina Nur Fauziah", "213040115", "vinaanurfauziah@gmail.com", 
+"Teknik Informatika"],
+["Muhammad Nazrililah", "Najril@gamil.com", "Teknik Informatika"]
+];
+
+?>
+
+<?php foreach($mahasiswa as $mhs) { ?>
+    <ul>
+        <li>Nama : <?php echo $mhs[0]; ?></li>
+        <li>NPM : <?php echo $mhs[1] ?></li>
+        <li>Email : <?php echo $mhs[2] ?> </li>
+        <li>Jurusan : <?php echo $mhs[3] ?></li>
+
+
+
+
+    </ul>
+
+<?php } ?>
